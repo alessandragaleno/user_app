d5f3d085f968e67505c36a432c0004638acfa4f6
alessandragaleno <alessandragaleno421@gmail.com>
2025-04-11 21:16:31 -0300

    perfi finalizado, com calendario e geneo adicionado

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        theme: ThemeData(
+      theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: LoginPage(),
@@ -25,33 +25,32 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _usuario = '';
+  String _senha = '';
 
-    final _formKey = GlobalKey<FormState>();
-    String _usuario = '';
-    String _senha = '';
-    
-    @override
-    void _login() {
-      if (_formKey.currentState!.validate()) {
-        _formKey.currentState!.save();
-        if (_usuario == 'alexya' && _senha == '1234'){
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => HomePage(usuario: _usuario)),
-          );
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Usuário ou senha inválidos')),
-          );
-        }
+  @override
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      if (_usuario == 'alexya' && _senha == '1234') {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage(usuario: _usuario)),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Usuário ou senha inválidos')),
+        );
       }
     }
+  }
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    backgroundColor: Colors.orange,
-      body: Center(   
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: Center(
         child: SingleChildScrollView(
           padding: EdgeInsets.all(16.0),
           child: Card(
@@ -59,33 +58,36 @@ class _LoginPageState extends State<LoginPage> {
               borderRadius: BorderRadius.circular(16.0),
             ),
             elevation: 10,
-            child: Padding(padding: EdgeInsets.all(20.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Icon(Icons.person, size: 100, color: Colors.orange),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Usuário',
-                      border: OutlineInputBorder(),
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(Icons.person, size: 100, color: Colors.orange),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Usuário',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) =>
+                          value!.isEmpty ? 'Digite o nome do usuário' : null,
+                      onSaved: (value) => _usuario = value!,
                     ),
-                    validator: (value)  => value!.isEmpty ? 'Digite o nome do usuário' : null,
-                    onSaved: (value) => _usuario = value!,
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      border: OutlineInputBorder(),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) =>
+                          value!.isEmpty ? 'Digite sua senha' : null,
+                      onSaved: (value) => _senha = value!,
                     ),
-                    validator: (value)  => value!.isEmpty ? 'Digite sua senha' : null,
-                    onSaved: (value) => _senha = value!,
-                  ),
-                  SizedBox(height: 20),
-                  SizedBox(
+                    SizedBox(height: 20),
+                    SizedBox(
                       width: double.infinity,
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
@@ -97,94 +99,101 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                         onPressed: _login,
                         child: Text('Entrar', style: TextStyle(fontSize: 18)),
-                      ),     
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ], 
-      ),
-    ),
-),
+          ),
         ),
       ),
-    ),
-  );
-        
+    );
   }
 }
 
-    //Classe Homepage que será a página de perfil de usuário
-    class HomePage extends StatelessWidget {
-      final String usuario;
+// Classe Homepage que será a página de perfil de usuário
+class HomePage extends StatefulWidget {
+  final String usuario;
 
-      HomePage({required this.usuario});
+  HomePage({required this.usuario});
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          backgroundColor: Colors.grey.shade100,
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: 200, //altura do cabeçalho
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.redAccent, Colors.orangeAccent, Colors.blue],
-                      begin: Alignment.topLeft, //Começa no topo a esquerda
-                      end: Alignment.bottomRight,//finaliza em baixo a direita
-            ),
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(30), //Arredonda o cabeçalho
-            ),
-          ),
-          child: Center(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.person, size: 80, color: Colors.grey),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: CircleAvatar(
-                      radius: 15,
-                      backgroundColor: Colors.black,
-                      child: Icon(Icons.edit, size: 15, color: Colors.white),
-                  ),
-                  ),
+class _HomePageState extends State<HomePage> {
+  DateTime? selectedDate;
+  String selectedGender = 'Selecione';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.redAccent, Colors.orangeAccent, Colors.blue],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(30),
+                ),
+              ),
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.person, size: 80, color: Colors.grey),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: CircleAvatar(
+                        radius: 15,
+                        backgroundColor: Colors.black,
+                        child: Icon(Icons.edit, size: 15, color: Colors.white),
+                      ),
+                    ),
                   ],
+                ),
+              ),
             ),
-            ),
-          ),
-          SizedBox(height: 20),
-           _buildTextField(label: "Nome", value: usuario), // Campo de entrada para o primeiro nome
-            _buildTextField(label: "Sobrenome", value: "Silva"), // Campo de entrada para o sobrenome
-            _buildOption(label: "nascimento"), // Opção para data de nascimento
-            _buildOption(label: "Genero"), // Opção para gênero
-            SizedBox(height: 20), // Espaçamento
-            _buildButton(text: "Alterar Senha", color: Colors.black), // Botão para alterar senha
-            _buildButton(text: "Sair", color: Colors.red, isOutlined: true), // Botão para logout
-              ],
+            SizedBox(height: 20),
+            _buildTextField(label: "Nome", value: widget.usuario),
+            _buildTextField(label: "Sobrenome", value: "Silva"),
+            _buildDatePicker(),
+            _buildGeneroDropDown(),
+            Divider(thickness: 1, indent: 20, endIndent: 20),
+            SizedBox(height: 20),
+            _buildButton(text: "Alterar Senha", color: Colors.black),
+            _buildButton(text: "Sair", color: Colors.red, isOutlined: true),
+          ],
         ),
       ),
     );
-   }
-   // Método para criar um campo de entrada de texto
+  }
+
   Widget _buildTextField({required String label, required String value}) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5), // Define o espaçamento externo
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
       child: TextFormField(
-        initialValue: value, // Define o valor inicial do campo
+        initialValue: value,
         decoration: InputDecoration(
-          labelText: label, // Define o rótulo do campo
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)), // Adiciona borda arredondada
+          labelText: label,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
         ),
       ),
     );
-  }     
-  
+  }
+
   Widget _buildOption({required String label}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
@@ -196,8 +205,9 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-  
-  Widget _buildButton({required String text, required Color color, bool isOutlined = false}) {
+
+  Widget _buildButton(
+      {required String text, required Color color, bool isOutlined = false}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: SizedBox(
@@ -207,7 +217,7 @@ class _LoginPageState extends State<LoginPage> {
           style: ElevatedButton.styleFrom(
             backgroundColor: isOutlined ? Colors.white : color,
             foregroundColor: isOutlined ? color : Colors.white,
-            side: isOutlined ? BorderSide(color : color) : null,
+            side: isOutlined ? BorderSide(color: color) : null,
             padding: EdgeInsets.symmetric(vertical: 15),
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
           ),
@@ -216,5 +226,57 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-}
 
+  Widget _buildDatePicker() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      child: ListTile(
+        title: Text('Aniversário', style: TextStyle(fontSize: 16)),
+        subtitle: Text(selectedDate != null
+            ? "${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}"
+            : "selecione sua data de aniversário"),
+        trailing: Icon(Icons.calendar_today, size: 18, color: Colors.grey),
+        tileColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        onTap: () async {
+          DateTime? pickedDate = await showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(1900),
+            lastDate: DateTime.now(),
+          );
+          if (pickedDate != null) {
+            setState(() {
+              selectedDate = pickedDate;
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildGeneroDropDown() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      child: DropdownButtonFormField<String>(
+        value: selectedGender,
+        items: ["Selecione", "Masculino", "feminino", "Trans", "Não binário", "Outro"]
+            .map((String gender) {
+          return DropdownMenuItem<String>(
+            value: gender,
+            child: Text(gender),
+          );
+        }).toList(),
+        onChanged: (String? newValue) {
+          setState(() {
+            selectedGender = newValue!;
+          });
+        },
+        decoration: InputDecoration(
+          labelText: 'Gênero',
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+        ),
+      ),
+    );
+  }
+}
